cf0a5407f54350983b5f50d9f7e92f32d5de51ff
Thanapoom Chusorn <thanapoom-c@rmutp.ac.th>
2025-09-03 14:13:49 +0700

    Initial commit on new branch

--- app/dist/index.js
+++ app/dist/index.js
@@ -5,7 +5,7 @@ const app = new hono_1.Hono();
 app.get("/", (c) => c.text("Hello World!"));
 app.get("/about", (c) => {
     return c.json({
-        mwssage: "Anupong Janjaroen"
+        mwssage: "Thanapoom Chusorn"
     });
 });
 exports.default = app;
--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,20 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World!"));
 
 app.get("/about", (c) => {
-    return c.json ({
-        mwssage: "Thanapoom Chusorn"
+    return c.json({
+        message: "Thanapoom Chusorn"
     });
+});
+
+app.get("/profile", async (c) => {
+    const profile = await prisma.profile.findMany();
+    return c.json(profile);
 });
 
 export default app;
